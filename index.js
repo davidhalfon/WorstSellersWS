9bea18f7aedf3dd7431c9a0842dc67ad1bf84b28
davidhalfon <halfon7@gmail.com>
2015-06-14 17:20:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var func = require('./functions.js');
 var PORT = process.env.PORT || 3000;
 app.set ('port' , PORT);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(app.get('port'), function(){
 	console.log('Connection Strated');
 });
